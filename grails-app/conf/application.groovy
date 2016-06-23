ce5cc1efb95cced8ac8a53d34dcc2ede5e6d8713
filasy <jfilasy@gmail.com>
2016-06-23 21:34:28 +0300

    posgres

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -28,14 +28,15 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
 
+
 environments {
 	production {
 		dataSource {
-			dbCreate = "create"
+			dbCreate = "create-drop"
 			driverClassName = "org.postgresql.Driver"
-			dialect = org.hibernate.dialect.PostgreSQL9Dialect
-			uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			uri = new URI(System.env.DATABASE_URL?:"postgres://localhost:5432/test")
+			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require"
 			username = uri.userInfo.split(":")[0]
 			password = uri.userInfo.split(":")[1]
 		}
