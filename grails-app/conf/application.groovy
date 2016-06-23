23d13d1c89b2d94dd0febcfeba9f296606c4a8d8
filasy <jfilasy@gmail.com>
2016-06-23 22:43:40 +0300

    posgres

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -34,7 +34,7 @@ environments {
 		dataSource {
 			dbCreate = "update"
 			driverClassName = "org.postgresql.Driver"
-			dialect = org.hibernate.dialect.PointbaseDialect
+			dialect = org.hibernate.dialect.PostgreSQLDialect
 			uri = new URI(System.env.DATABASE_URL?:"postgres://localhost:5432/test")
 			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path + "?sslmode=require"
 			username = uri.userInfo.split(":")[0]
