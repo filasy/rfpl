f52f7c108f7f72d08afd27b6a62a965ad5b1176e
filasy <jfilasy@gmail.com>
2016-06-23 13:22:46 +0300

    test database settings

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -28,3 +28,17 @@ grails.plugin.springsecurity.filterChain.chainMap = [
 	[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
 
+environments {
+	production {
+		dataSource {
+			dbCreate = "create-drop"
+			driverClassName = "org.postgresql.Driver"
+			dialect = org.hibernate.dialect.PostgreSQLDialect
+			uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+			url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+			username = uri.userInfo.split(":")[0]
+			password = uri.userInfo.split(":")[1]
+		}
+	}
+}
+
